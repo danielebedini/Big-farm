b5f3f435750d507f3c5b08d9c58afb799eb14d44
danielebedini <108469620+danielebedini@users.noreply.github.com>
2022-07-02 16:02:55 +0200

    Add files via upload
    Correzione imprecisioni su collector.py

--- collector.py
+++ collector.py
@@ -17,25 +17,22 @@ class ClientThread(threading.Thread):
     def run(self):
       handle_conn(self.conn,self.addr)
       
-# main
+
 def main(host=HOST, port=PORT):
-  # creo il server socket
+  # create socket
   with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    # unisco indirizzo e porta e sto in attesa
+    # bind port and address then wait
     s.bind((HOST,PORT))
     s.listen()
     while True:
       print("Waiting...")
-      # dopo che ricevo la connessione faccio partire i thread
+      # start threads after accepting connection
       conn, addr=s.accept()
-      # TODO distinguere le varie richieste da MW e Client
       t = ClientThread(conn,addr)
       t.start()
     s.shutdown(socket.SHUT_RDWR)
 
 def handle_conn(conn,addr):
-  # ogni richiesta prevede l'ordinamento crescente per somma
-  # print(f"Request by {addr}")
   # Before read string length
   temp = recv_all(conn,4)
   req_len = struct.unpack("!i", temp)[0]
@@ -53,44 +50,48 @@ def handle_conn(conn,addr):
   # Req. 1 (MW)
   if req.__eq__("MW"):
     mw_req(conn,addr,sum,fn)
-  # Req. 2 (Client)
+  # Req. 2 (C1)
   elif req.__eq__("C1"):
     cl_req1(conn,addr)
-  # Req. 3 (Client)
+  # Req. 3 (C2)
   elif req.__eq__("C2"):
-    cl_req2(conn,addr,sum,fn)
+    cl_req2(conn,addr,sum)
     
 def mw_req(conn,addr,sum,fn): # DONE and TESTED
-  # Req. 1 (MW): non risponde, memorizza una data coppia somma|nomefile
-  # print("Req. by MW. Sending...")
+  # Req. 1 (MW): store a given couple (sum, filename) in the couple list c_list
   c_list.append((sum,fn))
   c_list.sort()
   print("List: ",c_list)
   
-def cl_req1(conn,addr):
-  # Req. 2 (Client): elencare tutte le coppie somma|nomefile
+def cl_req1(conn,addr): # TODO: test this
+  # Req. 2 (C1): send all the couples (sum, filename)
   print("Req. C1: Sending...")
-  # send the global list back to the client
-  # first sends the length of the list, then every couple sum|filename
+  # first sends the length of the list, then every couple (sum, filename)
   conn.sendall(struck.pack("!i", len(c_list)))
-  for c in c_list:
-    conn.sendall(struck.pack("!i", c[0]))
-    conn.sendall(c[1].encode())
+  for sum,fn in c_list:
+    conn.sendall(struck.pack("!i", sum))
+    conn.sendall(fn.encode())
   
-def cl_req2(conn,addr, sum, fn):
-  # Req. 3 (Client): elencare le coppie somma|nomefile con una certa somma
+def cl_req2(conn,addr,new_sum): # TODO: test this
+  # Req. 3 (C2): send every couple (sum, filename) with a given sum, send "Nessun file" if there are no couples with that given sum
   print("Req. C2: Sending...")
   new_l=[]
-  for c in c_list:
-    if c.sum == sum:
+  for sum,fn in c_list:
+    if new_sum == sum:
       new_l.append((sum,fn))
-  # TODO: send the new list back to the client
-  for nc in new_l:
-    conn.sendall(struck.pack("!i", nc[0]))
-    conn.sendall(nc[1].encode())
+  if len(new_l)>0:
+    new_l.sort()
+  # send length first then send couples
+    conn.sendall(struck.pack("!i", len(new_l)))
+    for sum,fn in new_l:
+      conn.sendall(struck.pack("!i", sum))
+      conn.sendall(fn.encode())
+  else:
+    # if the new list is empty
+    conn.sendall("Nessun file".encode())
 
 def recv_all(conn,n):
-  chunks = b''  #b sta per byte
+  chunks = b''
   bytes_recd=0
   while bytes_recd < n:
     chunk=conn.recv(min(n-bytes_recd, 1024))
