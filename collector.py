caa3005c255f61a6613739b810583ebf790db049
danielebedini <108469620+danielebedini@users.noreply.github.com>
2022-07-06 13:48:58 +0200

    Collector finito.
    Commenti migliorati.

--- collector.py
+++ collector.py
@@ -1,13 +1,17 @@
 #! /usr/bin/env python3
-#Server Collector che con l'uso di threads gestisce le richieste dei client
+# Server Collector: handles requests sent by the Workers and the Client.
+
+# Possible requests:
+#    MW: from MasterWorker, stores the given couple (sum, filename) in the couple list.
+#    C1: from Client, sends the entire list of couples 
+#    C2: from Client, sends the list of couples with given numbers
 
 import sys, struct, socket, threading
 
-# host e porta di default
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 59643  # Port to listen on (non-privileged ports are > 1023)
 
-c_list = []
+c_list = [] # global list of couples
 
 class ClientThread(threading.Thread):
     def __init__(self,conn,addr):
@@ -41,7 +45,8 @@ def handle_conn(conn,addr):
   
   # check request
   if req.__eq__("MW"):
-    # Req. 1 (MW)
+    # Request: MW
+    # this request needs the sum and the filename
     # read the sum one half at a time
     temp = recv_all(conn,8)
     sum = struct.unpack("!q", temp)[0]
@@ -53,26 +58,29 @@ def handle_conn(conn,addr):
     mw_req(conn,addr,sum,fn)
     
   elif req.__eq__("C1"):
-    # Req. 2 (C1)
+    # Request: C1
+    # this request doesn't need the sum nor the filename
     cl_req1(conn,addr)
   
   elif req.__eq__("C2"):
-    # Req. 3 (C2)
+    # Request: C2
     # this request only needs the sum
     temp = recv_all(conn,8)
     sum = struct.unpack("!q", temp)[0]
     cl_req2(conn,addr,sum)
     
-def mw_req(conn,addr,new_sum,new_fn): # DONE and TESTED
-  # Req. 1 (MW): store a given couple (sum, filename) in the couple list c_list
+def mw_req(conn,addr,new_sum,new_fn): 
+  # Request: MW 
+  # store a given couple (sum, filename) in the couple list c_list
   for sum,fn in c_list:
     if(new_fn == fn):
       c_list.remove((sum, fn))
   c_list.append((new_sum,new_fn))
   c_list.sort()
   
-def cl_req1(conn,addr): # DONE and TESTED 
-  # Req. 2 (C1): send all the couples (sum, filename)
+def cl_req1(conn,addr):  
+  # Request: C2
+  # send all the couples (sum, filename)
   print("Req. C1: Sending...")
   # first sends the length of the list, then every couple (sum, filename)
   conn.sendall(struct.pack("!i", len(c_list)))
@@ -82,16 +90,19 @@ def cl_req1(conn,addr): # DONE and TESTED
     conn.sendall(struct.pack("!i", len(fn)))
     conn.sendall(fn.encode())
   
-def cl_req2(conn,addr,new_sum): # DONE and TESTED
-  # Req. 3 (C2): send every couple (sum, filename) with a given sum, send "Nessun file" if there are no couples with that given sum
+def cl_req2(conn,addr,new_sum): 
+  # Request: MW 
+  # send every couple (sum, filename) with a given sum 
+  # send 0 if there are no couples with that given sum
   print("Req. C2: Sending...")
-  new_l=[]
+  new_l=[] 
   for sum,fn in c_list:
     if new_sum == sum:
       new_l.append((sum,fn))
   if len(new_l)>0:
+    # new_l not empty
     new_l.sort()
-  # send length first then send couples
+    # send new_l length first then send couples with the sum given
     conn.sendall(struct.pack("!i", len(new_l)))
     for sum,fn in new_l:
       conn.sendall(struct.pack("!i", len(str(sum))))
@@ -99,6 +110,7 @@ def cl_req2(conn,addr,new_sum): # DONE and TESTED
       conn.sendall(struct.pack("!i", len(fn)))
       conn.sendall(fn.encode())
   else:
+    # new_l is empty, send 0
     conn.sendall(struct.pack("!i", len(new_l)))
   
 def recv_all(conn,n):
@@ -107,7 +119,7 @@ def recv_all(conn,n):
   while bytes_recd < n:
     chunk=conn.recv(min(n-bytes_recd, 1024))
     if len(chunk)==0:
-      raise RuntimeError("Socket connection broken")
+      raise RuntimeError("Socket connection broken.")
     chunks+=chunk
     bytes_recd+= len(chunk)
   return chunks
